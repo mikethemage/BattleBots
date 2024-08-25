a7649da155dd9d918e69808427d6bf0e1e94a033
Mike Dunn <44652427+mikethemage@users.noreply.github.com>
2024-08-25 12:20:22 +0100

    Added different round types, code to disable and re-enable buttons

--- arenaclient/scripts.js
+++ arenaclient/scripts.js
@@ -3,9 +3,24 @@ $(document).ready(function () {
     const BASE_URL = config.baseUrl;
     const SPINNER_COUNT = config.spinnerCount;
 
+    $('#start120').click(function () {
+        callApi(`${BASE_URL}/start120`, 'GET');
+        startRound(120);        
+    });
+
     $('#start180').click(function () {
         callApi(`${BASE_URL}/start180`, 'GET');
-        disableButton($('#start180'), 180);
+        startRound(180);        
+    });
+
+    $('#startduck').click(function () {
+        callApi(`${BASE_URL}/startduck`, 'GET');
+        startRound(75);        
+    });
+
+    $('#stopround').click(function () {
+        callApi(`${BASE_URL}/stop`, 'GET');        
+        stopUi();      
     });
 
     $('#flipper').click(function () {
@@ -40,6 +55,23 @@ $(document).ready(function () {
         }, timeoutSeconds * 1000);
     }
 
+    function stopUi() {
+        $('.start-button').prop('disabled', false);
+        $('.stop-button').prop('disabled', true);
+        $('.spinner-off').prop('checked',true);
+        $('.arena-hazard').prop('disabled', true);  
+    }
+
+    function startRound(length) {
+        $('.start-button').prop('disabled', true);
+        $('.stop-button').prop('disabled', false);
+        $('.arena-hazard').prop('disabled', false);
+
+        setTimeout(() => {
+            stopUi();
+        }, length * 1000);
+    }
+
     function controlSpinner(spinnerId, action) {
         let endpoint;
         switch (action) {
@@ -62,11 +94,11 @@ $(document).ready(function () {
                         <div class="col-12 col-md-4 text-center">
                             <h5>Spinner ${i}</h5>
                             <div class="btn-group btn-group-sm w-100" role="group" aria-label="Spinner ${i} Control">
-                                <input type="radio" class="btn-check" name="spinner${i}" id="spinner${i}-off" autocomplete="off" checked>
+                                <input type="radio" class="btn-check arena-hazard spinner-off" name="spinner${i}" id="spinner${i}-off" autocomplete="off" checked disabled>
                                 <label class="btn btn-outline-danger w-100 mb-1" for="spinner${i}-off">Off</label>
-                                <input type="radio" class="btn-check" name="spinner${i}" id="spinner${i}-clockwise" autocomplete="off">
+                                <input type="radio" class="btn-check arena-hazard spinner-on" name="spinner${i}" id="spinner${i}-clockwise" autocomplete="off" disabled>
                                 <label class="btn btn-outline-primary w-100 mb-1" for="spinner${i}-clockwise">Clockwise</label>
-                                <input type="radio" class="btn-check" name="spinner${i}" id="spinner${i}-counterclockwise" autocomplete="off">
+                                <input type="radio" class="btn-check arena-hazard spinner-on" name="spinner${i}" id="spinner${i}-counterclockwise" autocomplete="off" disabled>
                                 <label class="btn btn-outline-primary w-100 mb-1" for="spinner${i}-counterclockwise">Counter-Clockwise</label>
                             </div>
                         </div>
